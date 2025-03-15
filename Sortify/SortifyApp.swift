ef86c5f0d075c6b002c7bf899542c7ef4636b0bf
mido mj <abo.ashraf169@gmail.com>
2025-03-15 15:37:21 +0200

    Initial Commit

---
+++ Sortify/SortifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortifyApp.swift
+//  Sortify
+//
+//  Created by mido mj on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct SortifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
